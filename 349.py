1664114eb6a400d9c4e93d438e9699207c3183b0
Marcus Yu <marcusyuzc1992@gmail.com>
2016-05-15 17:56:09 -0500

    generated csv file and added csv writting function at 349.py

--- 349.py
+++ 349.py
@@ -1,10 +1,10 @@
 
 from numpy import *
-
+import csv
 # load the data
 def load_data():
     # load data and split the match up attribute 
-    data = matrix(genfromtxt('2015_2016 FORMER.csv', dtype = None , delimiter=','))
+    data = genfromtxt('2015_2016 FORMER.csv', dtype = None , delimiter=',')
     attri = []
     for row in range(0,data.shape[0]):
     	attri.append(data[row,1].split())
@@ -18,16 +18,25 @@ def combine_data(data,match_up):
 		new_row = []
 		for row_2 in range(row_1,len(data)):
 			if match_up[row_1][0] == match_up[row_2][2] and data[row_1][2] == data[row_2][2]:
-				new_row.append(data[row_1][2:])
+				new_row.extend(data[row_1][2:])
 				new_row.extend(data[row_2][4:])	
 		if new_row:
 			new_data.append(new_row)
  	return new_data
 
+
+def write_csv(data):
+	csvfile = file('2015-2016.csv','wb')
+	writer = csv.writer(csvfile)
+	for row in data:
+		writer.writerow(row)
+	csvfile.close()
+
+
 def main():
 	data,match_up = load_data()
 	new_data = combine_data(data,match_up)
-	print new_data
+	write_csv(new_data)
 	
 	# savetxt(
  #    '2015-2016.csv',           # file name
